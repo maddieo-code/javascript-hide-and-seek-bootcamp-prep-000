e2c03a970c08770993ff6b3070da7ddb5fafbd06
Maddie O'Meara <maddie.omeara@gmail.com>
2017-10-02 17:58:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function getFirstSelector(selector) {
   const firstSel = document.querySelector(selector);
   return firstSel;
 }
+
+function nestedTarget() {
+  
+}
+
+function increaseRankBy(n) {
+  const oneMore = document.querySelectorAll('ul.ranked-lists li')
+
+  for (var i = 0; i < oneMore.length; i++) {
+    oneMore[i].innerHTML = parseInt(i + 1);
+  }
+}
